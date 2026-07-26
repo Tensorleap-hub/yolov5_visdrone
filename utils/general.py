eba4c98c708f4da49b596daf654865a90eb296e8
Amit Cohen <amit.cohen@tensorleap.ai>
2026-07-26 11:45:27 +0300

    Removed broken link font that is not needed anyway

--- utils/general.py
+++ utils/general.py
@@ -521,7 +521,7 @@ def check_dataset(data, autodownload=True):
             dt = f'({round(time.time() - t, 1)}s)'
             s = f"success ✅ {dt}, saved to {colorstr('bold', root)}" if r in (0, None) else f"failure {dt} ❌"
             LOGGER.info(f"Dataset download {s}")
-    check_font('Arial.ttf' if is_ascii(data['names']) else 'Arial.Unicode.ttf', progress=True)  # download fonts
+    #check_font('Arial.ttf' if is_ascii(data['names']) else 'Arial.Unicode.ttf', progress=True)  # download fonts
     return data  # dictionary
 
 
